69751bde6e977372101b674fc5aa81d67939f353
Mike Turner <mike.turner@zenotech.com>
2020-10-30 15:29:34 +0000

    update to build files

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@
 
 import setuptools
 
+
+from get_version import get_version
+__version__ = get_version(__file__)
+del get_version
+
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(version=__version__)
